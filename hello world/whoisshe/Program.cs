6c76fd69a6b2c41317ed77b6f08e6f1df1a12ba5
Sebastián galvis <galviss131@outlook.com>
2026-02-12 14:59:44 -0500

    who is she

---
+++ hello world/whoisshe/Program.cs
@@ -0,0 +1,16 @@
+﻿using Shared;
+
+do
+{ 
+    var a = consoleExtension.GetInt("Ingrese primer número:");
+    var b = consoleExtension.GetInt("Ingrese segundo número:");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es multiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es multiplo de {b}");
+    }
+
+} while (true);
