a2b4909c23c790ca6a7065263133c3355dab1864
Sebastián galvis <galviss131@outlook.com>
2026-02-12 13:26:22 -0500

    the mix

---
+++ hello world/themix/Program.cs
@@ -0,0 +1,55 @@
+﻿using Shared;
+
+do
+{
+    Console.WriteLine("Ingrese 3 números diferentes");
+    var a = consoleExtension.GetInt("Ingrese primer número:");
+    var b = consoleExtension.GetInt("Ingrese segundo número:");
+    if (a == b)
+    {
+        Console.WriteLine("Deben ser números diferentes, vuelvelo a intentar...");
+        continue;
+    }
+    var c = consoleExtension.GetInt("Ingrese tercer número:");
+    if (b == c || c == a)
+    {
+        Console.WriteLine("Deben ser números diferentes, vuelvelo a intentar...");
+        continue;
+    }
+
+    if (a > b && a > c)
+    {
+        if (b > c)
+        {
+            Console.WriteLine($"El mañor es {a}, el medio es {b}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El mañor es {a}, el medio es {c}, el menor es {b}");
+        }
+    }
+    else if (b > a && b > c)
+    {
+        if (a > c)
+        {
+            Console.WriteLine($"El mañor es {b}, el medio es {a}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El mañor es {b}, el medio es {c}, el menor es {a}");
+        }
+    }
+    else
+    {
+        if (a > b)
+        {
+            Console.WriteLine($"El mañor es {c}, el medio es {a}, el menor es {b}");
+        }
+        else
+        {
+            Console.WriteLine($"El mañor es {c}, el medio es {b}, el menor es {a}");
+        }
+    }
+
+
+} while (true);
