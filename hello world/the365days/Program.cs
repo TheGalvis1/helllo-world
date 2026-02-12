95e3277cb69b95325607df822789745239ce9d42
Sebastián galvis <galviss131@outlook.com>
2026-02-12 15:37:23 -0500

    the 365 days

---
+++ hello world/the365days/Program.cs
@@ -0,0 +1,44 @@
+﻿using Shared;
+
+do
+{
+    var thepresent = DateTime.Now.Year;
+    var message = string.Empty;
+    var Year = consoleExtension.GetInt("Ingrese año: ");
+
+    if (Year == thepresent)
+    {
+        message = "es";
+    } else if (Year > thepresent)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+
+    if (Year % 4 == 0)
+    {
+        if (Year % 100 == 0)
+        {
+            if (Year % 400 == 0)
+            {
+                Console.WriteLine($"El año: {Year}, Si {message} biciesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año: {Year}, No {message} biciesto.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año: {Year}, Si {message} biciesto.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"el año: {Year}, No {message} biciesto.");
+    }
+} while (true);
